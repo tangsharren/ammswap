1154fb02fc00e7a5909437e1900690d5b6da5cd5
tangsharren <81765622+tangsharren@users.noreply.github.com>
2023-12-27 15:07:37 +0800

    Update XYKPool.sol

--- contracts/XYKPool.sol
+++ contracts/XYKPool.sol
@@ -1,15 +1,15 @@
 // SPDX-License-Identifier: MIT
-pragma solidity 0.8.19;
+pragma solidity 0.8.20;
 
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/IERC20Metadata.sol";
-import "@openzeppelin/contracts/utils/math/SafeMath.sol";
+// import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 import "@openzeppelin/contracts/utils/math/Math.sol";
 import "@openzeppelin/contracts/utils/Address.sol";
 import "@openzeppelin/contracts/utils/Context.sol";
 
 contract XYKPool {
-    using SafeMath for uint256;
+    // using SafeMath for uint256;
     using Address for address;
 
     IERC20 public immutable token1;
