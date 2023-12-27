143838d877309db110da2edba31bd75bd9b41e0b
tangsharren <81765622+tangsharren@users.noreply.github.com>
2023-12-27 14:26:02 +0800

    Delete migrations/1_deploy_token.js

--- migrations/1_deploy_token.js
+++
@@ -1,9 +0,0 @@
-var Flare = artifacts.require("Flare");
-var Zenix = artifacts.require("Zenix");
-
-module.exports = function (deployer) {
-  const ownerAddress1 = "0xBFb9Ff9e5c4c5B9428798e13E2C7ba208fF2813f";
-  const ownerAddress2 = "0x3a372AeDE13eE052F36f15F9D0AB966EE6b39B59";
-  deployer.deploy(Flare, ownerAddress1);
-  deployer.deploy(Zenix, ownerAddress2);
-};
